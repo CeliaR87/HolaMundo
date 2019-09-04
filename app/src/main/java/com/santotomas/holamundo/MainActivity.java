1fc6abf6c6fe9179c9ae783e66356a8005b71a08
israeliqq <ramirezcelia87@gmail.com>
2019-09-03 23:13:36 -0400

    creando mi primer proyecto

---
+++ app/src/main/java/com/santotomas/holamundo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.santotomas.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
